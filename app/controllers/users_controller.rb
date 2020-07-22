939f46c687cd859fd627de57ade2eb10a204047f
gozmeister <samgorick.sg@gmail.com>
2020-07-22 17:54:43 -0500

    added secure password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,41 @@
 class UsersController < ApplicationController
 
   def create
-    p params
-    user = User.find_or_create_by(username: params[:username])
-    render json: user
+    user = User.find_by(username: user_params[:username])
+    if user
+      render json: { error: "This user has already signed up. Try logging in instead." }
+    else
+      user = User.create!(user_params)
+      render json: user
+    end
+  end
+
+  def login 
+    user = User.find_by(username: user_params[:username])
+    if user && user.authenticate(user_params[:password])
+      render json: user
+    else
+      render json: { error: 'Invalid username or password'}
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 
 end
+  
+    # def show
+    #   token = request.headers['Authorization'].split(' ').last
+    #   decoded_token = JWT.decode(token, 's3cret!', true, { algorithm: 'HS256' })
+    #   id = decoded_token.first['user_id']
+  
+    #   user = User.find(id)
+    #   if user
+    #     render json: user, include: [:transactions, :budgets, :savings]
+    #   else
+    #     render json: [ error: 'Please log in']
+    #   end
+    # end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, :notes, :tags, :notetags
+
+  post '/signup', to: 'users#create'
+  post '/login', to: 'users#login'
+  get '/login', to: 'users#show'
 end
