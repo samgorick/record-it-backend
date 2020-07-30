86574bb6ddfed671c336a97a559fc51fd21c207e
gozmeister <samgorick.sg@gmail.com>
2020-07-30 12:57:24 -0500

    added tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Tag.create([
+  {
+    "id": 2,
+    "name": "Interesting"
+  },
+  {
+    "id": 1,
+    "name": "Admin"
+  },
+  {
+    "id": 3,
+    "name": "Funny"
+  },
+  {
+    "id": 4,
+    "name": "To Do"
+  },
+  {
+    "id": 5,
+    "name": "Cute"
+  },
+  {
+    "id": 6,
+    "name": "Important"
+  },
+  {
+    "id": 7,
+    "name": "News"
+  },
+  {
+    "id": 8,
+    "name": "Science"
+  },
+  {
+    "id": 9,
+    "name": "Travel"
+  },
+  {
+    "id": 10,
+    "name": "History"
+  }
+])
